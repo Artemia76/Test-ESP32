09e9a3bae5a2a4cc890e1c28fe5c3ff5977b9578
Artemia76 <neophile76@gmail.com>
2024-11-05 23:31:30 +0100

    Change program to use SPIFFS Web files instead of static html code in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,193 +1,135 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <atomic>
 
 #include "env.h"
 
-#define PIN 48
-#define NUMPIXELS 1
+const int led = 48;
+const int capteurLuminosite = 10;
 
 // Replace with your network credentials
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
+const int DelayOn = 100;
+const int DelayOff = 1000;
+
+// Use atomic to share async mutlithreaded 
+std::atomic<double> Lum (1.0);
 
 // Set web server port number to 80
-WiFiServer server(80);
+AsyncWebServer server(80);
 
-// Variable to store the HTTP request
-String header;
+Adafruit_NeoPixel pixels(1, led, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYON 100
-#define DELAYOFF 1000
-
-//uint8_t R,G,B;
-bool Red,Green,Blue;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
-void setup() {
-  pixels.begin();
+void setup()
+{
+  //--------------------------------------------serial
   Serial.begin(115200);
-  // Connect to Wi-Fi network with SSID and password
+  while(!Serial){} // Wait until serial monitor is online
+  Serial.println("Starting Program");
+
+  //--------------------------------------------GPIO
+  pixels.begin();
+  pinMode(capteurLuminosite, INPUT);
+
+  //--------------------------------------------SPIFFS
+  if(!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  File root =  SPIFFS.open("/");
+  File file  = root.openNextFile();
+
+  while(file)
+  {
+    Serial.print("file: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+  //--------------------------------------------WIFI
+  WiFi.begin(ssid, password);
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
+    delay(500);
   }
+
   // Print local IP address and start web server
-  Serial.println("");
+  Serial.println("\n");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  //--------------------------------------------SERVER
+  server.on("/",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/w3.css",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/w3.css", "text/css");
+  });
+
+  server.on("/script.js",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+
+  server.on("/lireLuminosite",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    int val = analogRead(capteurLuminosite);
+    String luminosite = String(val);
+    request->send(200,"text/plain", luminosite);
+    Serial.println("Received lireLuminosite = " +luminosite);
+  });
+
+  server.on("/on",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    Lum = 1.0;
+    request->send(200);
+    Serial.println("Received ON.");
+  });
+
+  server.on("/off",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    Lum = 0.0;
+    request->send(200);
+    Serial.println("Received OFF.");
+  });
+
   server.begin();
-  Red = false;
-  Green = false;
-  Blue = false;
+  Serial.println("Server Online.");
 }
 
-void set_RGB (bool pRed, bool pGreen, bool pBlue)
+void set_RGB (double pRed, double pGreen, double pBlue, double pLum)
 {
   uint8_t R,G,B;
-  R= pRed?255:0;
-  G= pGreen?255:0;
-  B= pBlue?255:0;
+  R= static_cast<uint8_t>(pRed * pLum * 255.0);
+  G= static_cast<uint8_t>(pGreen * pLum * 255.0);
+  B= static_cast<uint8_t>(pBlue * pLum * 255.0);
   pixels.setPixelColor(0, pixels.Color(R, G, B));
   pixels.show();
 }
 
-void loop() {
-  WiFiClient client = server.available();   // Listen for incoming clients
-  pixels.clear();
-  /*for(int i=0; i<NUMPIXELS; i++) {
-    R=static_cast<uint8_t>(random(10));
-    G=static_cast<uint8_t>(random(10));
-    B=static_cast<uint8_t>(random(10));
-    pixels.setPixelColor(i, pixels.Color(R, G, B));
-    pixels.show();
-    delay(DELAYON);
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
-    delay(DELAYOFF);
-  }*/
-
-    if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /Red/on") >= 0) {
-              Serial.println("Red on");
-              Red = true;
-              set_RGB(Red,Green,Blue);
-            } else if (header.indexOf("GET /Red/off") >= 0) {
-              Serial.println("Red off");
-              Red = false;
-              set_RGB(Red,Green,Blue);
-            } else if (header.indexOf("GET /Green/on") >= 0) {
-              Serial.println("Green on");
-              Green = true;
-              set_RGB(Red,Green,Blue);
-            } else if (header.indexOf("GET /Green/off") >= 0) {
-              Serial.println("Green off");
-              Green = false;
-              set_RGB(Red,Green,Blue);
-            } else if (header.indexOf("GET /Blue/on") >= 0) {
-              Serial.println("Blue on");
-              Blue = true;
-              set_RGB(Red,Green,Blue);
-            } else if (header.indexOf("GET /Blue/off") >= 0) {
-              Serial.println("Blue off");
-              Blue = false;
-              set_RGB(Red,Green,Blue);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #800000; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for Red Color  
-            client.println("<p>Red " + String(Red) + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (Red==false) {
-              client.println("<p><a href=\"/Red/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/Red/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for Green Color  
-            client.println("<p>Green " + String(Green) + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (Green==false) {
-              client.println("<p><a href=\"/Green/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/Green/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            // Display current state, and ON/OFF buttons for Green Color  
-            client.println("<p>Blue " + String(Blue) + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (Blue==false) {
-              client.println("<p><a href=\"/Blue/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/Blue/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-
+void loop()
+{
+double R,G,B;
+pixels.clear();
+  R=static_cast<double>(random(255))/255.0;
+  G=static_cast<double>(random(255))/255.0;
+  B=static_cast<double>(random(255))/255.0;
+  set_RGB(R,G,B,Lum);
+  delay(DelayOn);
+  set_RGB(R,G,B,0.0);
+  delay(DelayOff);
 }
